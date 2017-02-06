31fe56fa11bd181f11aa5fe7122033d616d24bbb
Brian Acker <briancacker@gmail.com>
2017-02-05 20:48:39 -0500

    Create Android Studio Project with a basic front page

---
+++ AckMath-Android/app/src/main/java/acker/brian/ackmath_android/BaseActivity.java
@@ -0,0 +1,13 @@
+package acker.brian.ackmath_android;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ AckMath-Android/app/src/main/java/acker/brian/ackmath_android/HomeActivity.java
@@ -0,0 +1,16 @@
+package acker.brian.ackmath_android;
+
+import android.os.Bundle;
+
+/**
+ * Created by Brian on 2/5/2017.
+ */
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+    }
+
+}
