b74d67a22b7f9e930c756eb112924ffaa403349f
Brian Acker <backer@brightcove.com>
2017-03-02 19:09:40 -0500

    Validate bounds before attempting to start challenge activity

--- app/src/main/java/acker/brian/ackmath_android/FrontPageViewModel.java
+++ app/src/main/java/acker/brian/ackmath_android/FrontPageViewModel.java
@@ -1,10 +1,8 @@
 package acker.brian.ackmath_android;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import acker.brian.ackmath_android.challenge.ChallengeActivity;
 import acker.brian.ackmath_android.event.LaunchScreenEvent;
@@ -27,9 +25,17 @@ public class FrontPageViewModel {
     public void onClickBegin(View view) {
         if (!TextUtils.isEmpty(lowerBound.getText().toString()) && !TextUtils.isEmpty(upperBound.getText().toString())) {
             Bundle args = new Bundle();
-            args.putInt(ChallengeActivity.EXTRA_LOWER_BOUND, Integer.parseInt(lowerBound.getText().toString()));
-            args.putInt(ChallengeActivity.EXTRA_UPPER_BOUND, Integer.parseInt(upperBound.getText().toString()));
-            EventBus.getDefault().post(new LaunchScreenEvent(LaunchScreenEvent.ScreenType.SQUARE_ROOT_CHALLENGE, args));
+            int lower = Integer.parseInt(lowerBound.getText().toString());
+            int upper = Integer.parseInt(upperBound.getText().toString());
+            if (validateBounds(lower, upper)) {
+                args.putInt(ChallengeActivity.EXTRA_LOWER_BOUND, lower);
+                args.putInt(ChallengeActivity.EXTRA_UPPER_BOUND, upper);
+                EventBus.getDefault().post(new LaunchScreenEvent(LaunchScreenEvent.ScreenType.SQUARE_ROOT_CHALLENGE, args));
+            }
         }
+    }
+
+    public boolean validateBounds(int lower, int upper) {
+        return upper > 0 && lower > 0 && upper - lower > 0;
     }
 }
