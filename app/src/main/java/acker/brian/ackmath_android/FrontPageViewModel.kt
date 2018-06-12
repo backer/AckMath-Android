d4fd285d11cae978e3e587071c9b903518758114
Brian Acker <backer@brightcove.com>
2018-06-12 19:00:34 -0400

    Convert FrontPageViewModel from java to kotlin

---
+++ app/src/main/java/acker/brian/ackmath_android/FrontPageViewModel.kt
@@ -0,0 +1,33 @@
+package acker.brian.ackmath_android
+
+import acker.brian.ackmath_android.challenge.ChallengeFragment
+import acker.brian.ackmath_android.event.LaunchScreenEvent
+import acker.brian.ackmath_android.utils.TextUtils
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import org.greenrobot.eventbus.EventBus
+
+/**
+ * Created by Brian on 2/5/2017.
+ */
+
+class FrontPageViewModel(private val lowerBound: EditText, private val upperBound: EditText) {
+
+    fun onClickBegin(view: View) {
+        if (!TextUtils.isEmpty(lowerBound.text.toString()) && !TextUtils.isEmpty(upperBound.text.toString())) {
+            val args = Bundle()
+            val lower = Integer.parseInt(lowerBound.text.toString())
+            val upper = Integer.parseInt(upperBound.text.toString())
+            if (validateBounds(lower, upper)) {
+                args.putInt(ChallengeFragment.LOWER_BOUND, lower)
+                args.putInt(ChallengeFragment.UPPER_BOUND, upper)
+                EventBus.getDefault().post(LaunchScreenEvent(LaunchScreenEvent.ScreenType.SQUARE_ROOT_CHALLENGE, args))
+            }
+        }
+    }
+
+    fun validateBounds(lower: Int, upper: Int): Boolean {
+        return upper > 0 && lower > 0 && upper - lower > 0
+    }
+}
